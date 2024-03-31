31309b3af8c78ee83a71ef7246638f1650ca2408
juanpablo1997 <juanpabloorozco15@gmail.com>
2024-03-31 18:39:45 -0500

    Se agregan estilos para el detalle de las ofertas

--- src/components/integrated/detailsJobs/DetailsJobs.jsx
+++ src/components/integrated/detailsJobs/DetailsJobs.jsx
@@ -1,13 +1,39 @@
 import css from "../detailsJobs/DetailsJobs.module.css";
 import { useContext } from "react";
 import { MyContext } from "../../../context/Context";
+import SubTitle from "../../core/subTitles/SubTitle";
 
 const DetailsJobs = () => {
   const { selectOffer } = useContext(MyContext);
 
   return (
     <div className={css.container}>
-      <p>{selectOffer.title}</p>
+      <div>
+        <SubTitle subTitleType="bigTitle" value={selectOffer.title} />
+        <p>{selectOffer.city}</p>
+      </div>
+      <hr />
+      <div>
+        <br />
+        <p>
+          Somos una empresa líder en [industria/nicho], comprometida con la
+          excelencia y la innovación. Estamos buscando talento apasionado y
+          motivado para unirse a nuestro equipo en [ubicación].
+        </p>
+        <br />
+        <SubTitle value="Requisitos" />
+        <p>- Excelentes habilidades</p>
+        <p>{`- Experiencia previa de ${selectOffer.experience}`}</p>
+        <p>- Capacidad para trabajar en equipo</p>
+        <br />
+      </div>
+      <div>
+        <p>
+          Si estás listo para enfrentar nuevos desafíos y contribuir al éxito de
+          una empresa en crecimiento, ¡esperamos recibir tu solicitud! Por
+          favor, envía tu currículum vitae y carta de presentació
+        </p>
+      </div>
     </div>
   );
 };
